2a3c7f342aa73881f02d88b6a1becd40aec8cb5e
Marc David <7226788+weekywhawha@users.noreply.github.com>
2023-06-27 23:29:49 +0100

    fix: add flash plugin

--- .config/nvim/lua/weekywhawha/plugins-setup.lua
+++ .config/nvim/lua/weekywhawha/plugins-setup.lua
@@ -37,12 +37,12 @@ return lazy.setup({
 	"nvim-lualine/lualine.nvim",
 
 	-- fuzzy finding w/ telescope
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },  -- dependency for better sorting performance
-	{ "nvim-telescope/telescope.nvim",            branch = "0.1.x" }, -- fuzzy finder
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- dependency for better sorting performance
+	{ "nvim-telescope/telescope.nvim", branch = "0.1.x" }, -- fuzzy finder
 
 	-- netwr-like file browser
 	{
-		'stevearc/oil.nvim',
+		"stevearc/oil.nvim",
 		opts = {},
 		-- Optional dependencies
 		dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -51,29 +51,32 @@ return lazy.setup({
 	"nvim-telescope/telescope-ui-select.nvim",
 
 	-- autocompletion
-	"hrsh7th/nvim-cmp",  -- completion plugin
+	"hrsh7th/nvim-cmp", -- completion plugin
 	"hrsh7th/cmp-buffer", -- source for text in buffer
-	"hrsh7th/cmp-path",  -- source for file system paths
+	"hrsh7th/cmp-path", -- source for file system paths
 
 	-- snippets
-	"L3MON4D3/LuaSnip",            -- snippet engine
-	"saadparwaiz1/cmp_luasnip",    -- for autocompletion
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	"saadparwaiz1/cmp_luasnip", -- for autocompletion
 	"rafamadriz/friendly-snippets", -- useful snippets
 
 	-- managing & installing lsp servers, linters & formatters
-	"williamboman/mason.nvim",          -- in charge of managing lsp servers, linters & formatters
+	"williamboman/mason.nvim", -- in charge of managing lsp servers, linters & formatters
 	"williamboman/mason-lspconfig.nvim", -- bridges gap b/w mason & lspconfig
 
 	-- configuring lsp servers
-	"neovim/nvim-lspconfig",             -- easily configure language servers
-	"hrsh7th/cmp-nvim-lsp",              -- for autocompletion
+	"neovim/nvim-lspconfig", -- easily configure language servers
+	"hrsh7th/cmp-nvim-lsp", -- for autocompletion
 
 	"jose-elias-alvarez/typescript.nvim", -- additional functionality for typescript server (e.g. rename file & update imports)
-	"onsails/lspkind.nvim",              -- vs-code like icons for autocompletion
+	"onsails/lspkind.nvim", -- vs-code like icons for autocompletion
 
 	-- formatting & linting
 	"jose-elias-alvarez/null-ls.nvim", -- configure formatters & linters
-	"jayp0521/mason-null-ls.nvim",    -- bridges gap b/w mason & null-ls
+	"jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
 
 	-- treesitter configuration
 	{
@@ -92,11 +95,44 @@ return lazy.setup({
 
 	-- git integration
 	"lewis6991/gitsigns.nvim", -- show line modifications on left hand side
-	"tpope/vim-fugitive",     -- git plugin
+	"tpope/vim-fugitive", -- git plugin
 
 	-- diagnostics, references, qflist, etc...
 	{
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	-- search labels navigation
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					-- default options: exact mode, multi window, all directions, with a backdrop
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+		},
 	},
 })
