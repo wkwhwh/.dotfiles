ce5ff1dea1974c29cb79852bef55b6bde7075eb3
Marc David <7226788+wkwhwh@users.noreply.github.com>
2024-06-09 11:57:57 +0100

    fix: wezterm and tmux theme cleanup

--- .config/nvim/lua/wkwhwh/plugins/lualine.lua
+++ .config/nvim/lua/wkwhwh/plugins/lualine.lua
@@ -3,15 +3,46 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
-		lualine.setup {
+
+		lualine.setup({
+			options = { section_separators = '', component_separators = '' },
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+					"branch",
+					"diff",
+					{ "diagnostics", symbols = { error = " ", warn = " ", info = " ", hint = "󰠠 " } },
+				},
+				lualine_c = {
+					{ 'filename', path = 4 },
+				},
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
 			tabline = {
-				lualine_a = { 'buffers' },
+				lualine_a = { {
+					'buffers',
+					filetype_names = {
+						TelescopePrompt = 'Telescope',
+						dashboard = 'Dashboard',
+						alpha = 'Alpha',
+						oil = 'Oil',
+						mason = 'Mason',
+						lazy = 'Lazy'
+					},
+					symbols = {
+						modified = ' ●', -- Text to show when the buffer is modified
+						alternate_file = '# ', -- Text to show to identify the alternate file
+						directory = '', -- Text to show when the buffer is a directory
+					},
+				} },
 				lualine_b = {},
 				lualine_c = {},
 				lualine_x = {},
 				lualine_y = {},
-				lualine_z = {}
+				lualine_z = {},
 			},
-		}
-	end
+		})
+	end,
 }
--- .config/wezterm/theme.lua
+++ .config/wezterm/theme.lua
@@ -1,44 +1,5 @@
 local Theme = {}
 
-local themes = {
-  kanagawa = {
-    dark = {
-      rosewater = "#F5E0DC",
-      flamingo = "#F2CDCD",
-      pink = "#F5C2E7",
-      mauve = "#CBA6F7",
-      red = "#F38BA8",
-      maroon = "#EBA0AC",
-      peach = "#FAB387",
-      yellow = "#F9E2AF",
-      green = "#A6E3A1",
-      teal = "#94E2D5",
-      sky = "#89DCEB",
-      sapphire = "#74C7EC",
-      blue = "#89B4FA",
-      lavender = "#B4BEFE",
-      text = "#CDD6F4",
-      subtext1 = "#BAC2DE",
-      subtext0 = "#A6ADC8",
-      overlay2 = "#9399B2",
-      overlay1 = "#7F849C",
-      overlay0 = "#6C7086",
-      surface2 = "#585B70",
-      surface1 = "#45475A",
-      surface0 = "#313244",
-      base = "#1E1E2E",
-      mantle = "#181825",
-      crust = "#11111B",
-    },
-  },
-}
-
-local THEME_NAME = "kanagawa"
-
-local palettes = themes[THEME_NAME]
-
-Theme.palette = palettes.dark
-
 Theme.colors = {
   foreground = "#dcd7ba",
   background = "#1f1f28",
